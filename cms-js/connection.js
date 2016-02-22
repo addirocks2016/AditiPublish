68c8f5957799cc1389289112d08e7fd438da7608
addirocks2016 <addirocks2013@gmail.com>
2016-02-23 01:12:06 +0530

    gitana ticket

--- cms-js/connection.js
+++ cms-js/connection.js
@@ -6,6 +6,7 @@ var applicationId = "8428b6fe9176f694a8e5";
 talentwebsite:
 var repositoryId = '4453eed66766d424b14e';
 var branchId = '58be9297f2c3838bd487';
+var GITANA_TICKET= '1e1e224e-6ae4-4eb4-9795-8b0b00fa5f63_b%2FG4fT%2Fs0A0yJq7QiOp%2B2bLaHS8i1koOT8YmaCc9AkzOPPPqrNaWJ%2F%2BXOm%2Bff7JvI%2BsPnVPKgUf8%0AAd%2B3Ezatsy%2FdhJvPp%2BNpTlxOILiZlpVihzV9UlWCUCSKFhBQ2jhQMX4bAb6HYIAxXfH4uBNmtAf5%0AwCZAB93BiRxjLNr9jZKYNFLUJSCGGQ%3D%3D; path=/; domain=api.cloudcms.com; Secure';
 var repository;
 var branch;
 var newCommentId;
--- cms-js/custom.js
+++ cms-js/custom.js
@@ -3,8 +3,10 @@ var username = document.getElementById("Username").value;
 var password = document.getElementById("Password").value;
 var flag=0;	
 if (username == 'talentpool' && password == 'aonhewitt'){
+
 sessionStorage.setItem('username', username);
 sessionStorage.setItem('password', password);
+    sessionStorage.setItem('GITANA_TICKET', GITANA_TICKET);
 flag=1;
 //window.location = "adminmainpagelanding.html"; 
  //window.open('adminmainpagelanding.html')// Redirecting to other page.
